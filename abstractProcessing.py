af98609ce4b5f04951de91bf71e79e3046441e91
Ezra Ablaza <ezra.ablaza.inbox@gmail.com>
2020-07-26 03:34:15 -0500

    Retrieve the top 5 words mentioned after query (pre-stem)

--- abstractProcessing.py
+++ abstractProcessing.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.corpus import stopwords
 
 try: 
     nltk.data.find('tokenizers/punkt')
@@ -7,12 +8,24 @@ except LookupError:
 import json
 
 
+
 def analyze_results(data):
-    tokens = nltk.word_tokenize(data[0]['abstract'])
-    print(tokens)
+    result_tokens=[]
+    sr = stopwords.words('english')
+    for entry in data:
+        tokens = nltk.word_tokenize(entry['abstract'])
+        clean_tokens = [t for t in tokens if (t.isalnum() and t.lower() not in sr)]
+        result_tokens.extend(clean_tokens)
+
+    freq = nltk.FreqDist(result_tokens)
+    freq.plot(20, cumulative=False)
+    freq_as_dict = dict(freq)
+    top_five_after = {w: freq_as_dict[w] for w in sorted(freq_as_dict, key=freq_as_dict.__getitem__, reverse=True)[2:7]}
+    print(top_five_after)
 
 
 if __name__ == "__main__":
+
     with open('./results.json') as results:
         data = json.loads(results.read())
 
