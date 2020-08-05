1c8ecc962ad64c85c1de7ef84ec3a8fcd2116754
Ezra Ablaza <ezra.ablaza.inbox@gmail.com>
2020-08-05 16:57:00 -0500

    Leverage DB to cache searches

--- lambdas/arxiv-result/paper_retrieval.py
+++ lambdas/arxiv-result/paper_retrieval.py
@@ -5,15 +5,20 @@ API_URL = 'http://export.arxiv.org/api/query'
 
 
 
-def extract_abstracts(params):
-    response = requests.get(API_URL, params=params,timeout=3)
+def extract_papers(params):
+    """ Queries the Arxiv API using the given paarams and returns the parsed content """
+    response = requests.get(API_URL, params=params,timeout=5)
     if response.status_code != 404:
         results = feedparser.parse(response.content)
         return results
     else:
         return ""   
 
-def process_feed(results=None):
+def process_feed(results):
+    """ 
+    Transforms a feed of papers into a list of json objects with relevant attributes
+    of title, link, abstract, and authors
+    """
     entries = results.entries
     papers = []
     for entry in entries: 
